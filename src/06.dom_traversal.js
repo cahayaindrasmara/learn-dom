f6b2e18cf090baac444a870b032b690cebfd46ec
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-05-19 20:32:57 +0700

    learn prevent default, prevent bubbling and create image gallery

--- src/06.dom_traversal.js
+++ src/06.dom_traversal.js
@@ -5,7 +5,7 @@
 // })
 
 //dom traversal
-const closeElement = document.querySelectorAll(".close")
+// const closeElement = document.querySelectorAll(".close")
 // console.log(closeElement)
 // const card = document.querySelectorAll(".card")
 
@@ -18,16 +18,35 @@ const closeElement = document.querySelectorAll(".close")
 //     })
 // }
 
-closeElement.forEach(function (el) {
-    el.addEventListener("click", function (e) {
+// closeElement.forEach(function (el) {
+//     el.addEventListener("click", function (e) {
+//         e.target.parentElement.style.display = "none"
+//         e.preventDefault(); //mencegah aksi default di jalankan
+//         e.stopPropagation(); //digunakan untuk menangani event bubbling
+//     })
+// })
+
+// const cards = document.querySelectorAll(".card");
+// cards.forEach(function (el) {
+//     el.addEventListener("click", function (e) {
+//         alert("ok")
+//     })
+// })
+
+// const nama = document.querySelector(".nama");
+
+// console.log(nama.parentElement)
+// console.log(nama.nextSibling)
+// console.log(nama.nextElementSibling.nextElementSibling)
+// console.log(nama.previousElementSibling)
+
+const container = document.querySelector(".container");
+
+container.addEventListener("click", function (e) {
+    if (e.target.className === "close") {
+        // alert("ok")
         e.target.parentElement.style.display = "none"
-    })
+        e.preventDefault();
+    }
 })
-
-const nama = document.querySelector(".nama");
-
-console.log(nama.parentElement)
-console.log(nama.nextSibling)
-console.log(nama.nextElementSibling.nextElementSibling)
-console.log(nama.previousElementSibling)
 
